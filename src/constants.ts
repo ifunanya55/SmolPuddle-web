7e22b1cb562bd53896cebf0c9444f6cfbb74e817
D3G3N <42593114+ifunanya55@users.noreply.github.com>
2021-09-29 16:16:50 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,12 @@
 import { providers } from "@0xsequence/multicall"
 import { ethers } from "ethers"
 
-export const ARBITRUM_EXPLORER = "https://arbiscan.io/"
-export const ARBITRUM_DEFAULT_RPC = "https://arb1.arbitrum.io/rpc"
-export const ARBITRUM_CHAIN_ID = 42161
+export const ARBITRUM_EXPLORER = "https://smartscan.cash/"
+export const ARBITRUM_DEFAULT_RPC = "https://smartbch.greyh.at/"
+export const ARBITRUM_CHAIN_ID = 10000
 
-export const EXPLORER_ADDR = "https://arbiscan.io/"
-export const SmolPuddleContract = "0xa39eAd9429AB35bFA7aA85786bcddA500a78155D"
+export const EXPLORER_ADDR = "https://smartscan.cash/"
+export const SmolPuddleContract = "0xccCA17Bc63599025762F48D4B2Bb690C640C4239"
 export const STATIC_PROVIDER = new providers.MulticallProvider(
   new ethers.providers.JsonRpcProvider(ARBITRUM_DEFAULT_RPC),
   { batchSize: 100, timeWindow: 500, verbose: false }
